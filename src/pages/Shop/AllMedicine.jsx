f5b9ae530c18ba67107ec727fa5998e67232400f
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-01-21 17:00:30 +0600

    search filet complete

--- src/pages/Shop/AllMedicine.jsx
+++ src/pages/Shop/AllMedicine.jsx
@@ -1,64 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import AllMedicineRow from './AllMedicineRow';
 import LoadingSpinner from '../../components/Shared/LoadingSpinner';
+import { FaSearchengin } from 'react-icons/fa';
 const AllMedicine = () => {
+    const [search, setSearch] = useState('')
+
+
     const { data: medicine = [], isLoading, refetch } = useQuery({
-        queryKey: ['medicine'],
+        queryKey: ['medicine',search ],
         queryFn: async () => {
-            const { data } = await axios.get('http://localhost:5000/medicine');
+            const { data } = await axios.get(`http://localhost:5000/medicine?search=${search}`);
             return data;
-        }
+        },
+
     });
-    
+
     if (isLoading) return <LoadingSpinner />;
-    
+
+
     return (
         <div className=" overflow-x-auto  xl:mx-28 2xl:mx-36 lg:mx-10 sm:mx-5 mx-2 my-10">
             <div className='flex justify-between mb-8'>
                 <h1 className='text-xl' >( All Medicine <span className='text-primary' >{medicine.length}</span> )</h1>
-                
-            </div>
-        <table className="table">
-            {
-                medicine.length > 0 &&
-                <thead>
-                    <tr className='text-lg  text-neutral'>
-                        <th>Image</th>
-                        <th>Name</th>
-                        <th>Generic</th>
-                        <th>Category</th>
-                        <th>Company</th>
-                        <th>Details</th>
-                        <th className='text-end'>Cart</th>
+                <div className='flex p-1 overflow-hidden border rounded-lg focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
+                    <input
+                        className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent'
+                        type='text'
+                        name='search'
+                        placeholder='Seach foods'
+                        onBlur={(e) => setSearch(e.target.value)}
+                        
                        
+                    />
 
-                    </tr>
-                </thead>
-
-            }
-
-            {medicine.length === 0 &&
-                <div className="flex h-screen justify-center my-5">
-                    <div>
-                        <h1 className='text-4xl py-3 text-neutral'>No Data Found ?</h1>
-                        <Link to={'/allfoods'} > <button className='py-2 my-3 px-6 bg-primary-content text-primary rounded-md'>Purchase Food</button> </Link>
-                    </div>
+                    <button className='px-1 md:px-4 py-3 text-sm text-neutral '>
+                        <FaSearchengin size={25} />
+                    </button>
                 </div>
-            }
+            </div>
+            <table className="table">
+                {
+                    medicine.length > 0 &&
+                    <thead>
+                        <tr className='text-lg  text-neutral'>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Generic</th>
+                            <th>Category</th>
+                            <th>Company</th>
+                            <th>Details</th>
+                            <th className='text-end'>Cart</th>
+
+
+                        </tr>
+                    </thead>
+
+                }
+
+                {medicine.length === 0 &&
+                    <div className="flex h-screen justify-center my-5">
+                        <div>
+                            <h1 className='text-4xl py-3 text-neutral'>No Data Found ?</h1>
+                            <Link to={'/allfoods'} > <button className='py-2 my-3 px-6 bg-primary-content text-primary rounded-md'>Purchase Food</button> </Link>
+                        </div>
+                    </div>
+                }
 
 
 
 
-            {
-                medicine.map((medicine, index) =>
-                    <AllMedicineRow medicine={medicine} key={medicine?._id} refetch ={refetch}  index={index} ></AllMedicineRow>
-                )
-            }
-        </table>
-    </div>
+                {
+                    medicine.map((medicine, index) =>
+                        <AllMedicineRow medicine={medicine} key={medicine?._id} refetch={refetch} index={index} ></AllMedicineRow>
+                    )
+                }
+            </table>
+        </div>
     );
 };
 
