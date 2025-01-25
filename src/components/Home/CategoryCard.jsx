ffff742a243d876b74158ad4b93e4859742cc6dc
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-01-25 23:28:22 +0600

    count by category product

--- src/components/Home/CategoryCard.jsx
+++ src/components/Home/CategoryCard.jsx
@@ -1,57 +1,52 @@
 import React from 'react';
-import slider from '../../../src/assets/Medicine-banner-1.jpg';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import LoadingSpinner from '../Shared/LoadingSpinner';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 const CategoryCard = () => {
-   
-    const navigate = useNavigate()
-    const { data: category = [], isLoading, } = useQuery({
-        queryKey: ['category'],
-        queryFn: async () => {
-            const { data } = await axios.get('https://medi-mart-server-opal.vercel.app/category');
-            return data;
-        }
-    });
+   const navigate = useNavigate();
+   const { data: category = [], isLoading } = useQuery({
+      queryKey: ['category'],
+      queryFn: async () => {
+         const { data } = await axios.get('https://medi-mart-server-opal.vercel.app/category');
+         return data;
+      }
+   });
 
-    
-   
-    if (isLoading) return <LoadingSpinner />;
-    return (
-        <div>
-            <h1 className='text-center pt-6 text-4xl' >Shop By Category</h1>
-            <div className='grid max-w-7xl mx-auto my-12 lg:grid-cols-4 grid-cols-1 sm:grid-cols-3 xl:grid-cols-6 place-items-center gap-4'>
-                {
-                    category.slice(0,6).map(category =>
-                        <div 
-                        
-                        onClick={()=> navigate(`/category/${category.categoryName}`)} 
-                        
-                        class="flex justify-center items-center my-10  bg-gray-100">
-                            <div class="w-52 h-52 bg-white shadow-lg rounded-lg overflow-hidden hover:scale-105 transition-transform duration-300">
+   if (isLoading) return <LoadingSpinner />;
 
-                                <div class="h-[85%]">
-                                    <img
-                                        src={category.categoryImage}
-                                        alt="Category"
-                                        class="w-full h-full object-cover"
-                                    />
-                                </div>
-
-                                <div class="h-[15%] flex justify-center items-center bg-primary">
-                                    <h2 class="text-lg font-semibold text-white">{category.categoryName} </h2>
-                                </div>
-                               
-                            </div>
+   return (
+      <div>
+         <h1 className='text-center pt-6 text-4xl'>Shop By Category</h1>
+         <div className='grid max-w-7xl mx-auto my-12 lg:grid-cols-4 grid-cols-1 sm:grid-cols-3 xl:grid-cols-6 place-items-center gap-4'>
+            {
+               category.slice(0, 6).map((category, index) =>
+                  <div
+                     key={index}
+                     onClick={() => navigate(`/category/${category.categoryName}`)}
+                     className="flex justify-center items-center my-10 bg-gray-100"
+                  >
+                     <div className="w-52 h-52 bg-white shadow-lg rounded-lg overflow-hidden hover:scale-105 transition-transform duration-300">
+                        <div className="h-[85%]">
+                           <img
+                              src={category.categoryImage}
+                              alt="Category"
+                              className="w-full h-full object-cover"
+                           />
                         </div>
-                    )
-                }
 
-            </div>
-        </div>
-
-    );
+                        <div className="h-[15%] flex justify-center items-center bg-primary">
+                           <h2 className="text-lg font-semibold text-white">{category.categoryName}</h2>
+                           <span className="text-sm text-white ml-2">({category.productCount} Products)</span>
+                        </div>
+                     </div>
+                  </div>
+               )
+            }
+         </div>
+      </div>
+   );
 };
 
 export default CategoryCard;
